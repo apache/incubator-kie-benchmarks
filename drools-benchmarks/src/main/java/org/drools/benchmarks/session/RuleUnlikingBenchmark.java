0c53366db2dc5d180a414f129e264109faf9e4a2
Mario Fusco <mario.fusco@gmail.com>
2016-04-08 15:29:24 +0200

    RuleUnlikingBenchmark + UpdateFireLoopBenchmark

--- drools-benchmarks/src/main/java/org/drools/benchmarks/session/RuleUnlikingBenchmark.java
+++ drools-benchmarks/src/main/java/org/drools/benchmarks/session/RuleUnlikingBenchmark.java
@@ -18,16 +18,13 @@ package org.drools.benchmarks.session;
 
 import org.drools.benchmarks.domain.A;
 import org.drools.benchmarks.domain.B;
-import org.drools.benchmarks.domain.C;
-import org.drools.benchmarks.domain.D;
-import org.drools.benchmarks.domain.E;
 import org.kie.api.runtime.rule.FactHandle;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Setup;
 
-public class UpdateJoinRootFactAndFireBenchmark extends AbstractSessionBenchmark {
+public class RuleUnlikingBenchmark extends AbstractSessionBenchmark {
 
     @Param({"10", "100", "1000"})
     private int loopCount;
@@ -38,35 +35,19 @@ public class UpdateJoinRootFactAndFireBenchmark extends AbstractSessionBenchmark
     @Param({"1", "10", "100"})
     private int factsNr;
 
-    @Param({"true", "false"})
-    private boolean insertLastJoinItem;
-
     @Setup
     public void setupKieBase() {
         StringBuilder sb = new StringBuilder();
         sb.append( "import org.drools.benchmarks.domain.*;\n" );
-        sb.append( "rule R0 salience 10 when\n" +
-                "  $factA : A( $a : value > 0)\n" +
-                "  B( $b : value > $a)\n" +
-                "  C( $c : value > $b)\n" +
-                "  D( $d : value > $c)\n" +
-                "  E( $e : value > $d)\n" +
-                "then\n" +
-                "  modify( $factA ) { setValue(-1) }; \n" +
-                "end\n" );
-
-        for (int i = 1; i < rulesNr; i++) {
+        for ( int i = 0; i < rulesNr; i++ ) {
             sb.append( "rule R" + i + " when\n" +
-                    "  A( $a : value > " + i + ")\n" +
-                    "  B( $b : value > $a)\n" +
-                    "  C( $c : value > $b)\n" +
-                    "  D( $d : value > $c)\n" +
-                    "  E( $e : value > $d)\n" +
-                    "then\n" +
-                    "end\n" );
+                       "  A( $a : value > " + i + ")\n" +
+                       "  B( $b : value > $a)\n" +
+                       "then\n" +
+                       "end\n" );
         }
 
-        createKieBaseFromDrl(sb.toString());
+        createKieBaseFromDrl( sb.toString() );
     }
 
     @Setup(Level.Iteration)
@@ -77,20 +58,17 @@ public class UpdateJoinRootFactAndFireBenchmark extends AbstractSessionBenchmark
 
     @Benchmark
     public void test() {
-        A a = new A( -1 );
-        FactHandle aFH = kieSession.insert( a );
-        for (int i = 0; i < factsNr; i++) {
+        FactHandle aFH = kieSession.insert( new A( rulesNr + 1 ) );
+        for ( int i = 0; i < factsNr; i++ ) {
             kieSession.insert( new B( rulesNr + 3 ) );
-            kieSession.insert( new C( rulesNr + 5 ) );
-            kieSession.insert( new D( rulesNr + 7 ) );
-            if (insertLastJoinItem) {
-                kieSession.insert( new E( rulesNr + 9 ) );
-            }
         }
 
+        kieSession.fireAllRules();
+
         for (int i = 0; i < loopCount; i++) {
-            a.setValue( 1 );
-            kieSession.update( aFH, a );
+            kieSession.delete( aFH );
+            kieSession.fireAllRules();
+            kieSession.insert( new A( rulesNr + 1 ) );
             kieSession.fireAllRules();
         }
     }
