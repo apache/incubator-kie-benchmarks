29726ef6f083ba470fad8624499a3e80c05e21ee
Tibor Zimanyi <tibor.zimanyi@gmail.com>
2017-04-11 12:41:36 +0200

    Add OOPath comparative benchmarks (#10)
    Add benchmarks that replace class OOPathBenchmarkTest from
    Drools repository

---
+++ drools-benchmarks/src/main/java/org/drools/benchmarks/oopath/AbstractOOPathComparisonBenchmark.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.benchmarks.oopath;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.drools.benchmarks.common.AbstractBenchmark;
+import org.drools.benchmarks.common.ProviderException;
+import org.drools.benchmarks.oopath.model.Child;
+import org.drools.benchmarks.oopath.model.Man;
+import org.drools.benchmarks.oopath.model.Toy;
+import org.drools.benchmarks.oopath.model.Woman;
+import org.drools.core.common.InternalFactHandle;
+import org.kie.api.runtime.KieSession;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.Warmup;
+
+@Warmup(iterations = 40000)
+@Measurement(iterations = 10000)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+public abstract class AbstractOOPathComparisonBenchmark extends AbstractBenchmark {
+
+    protected static final String MODEL_PACKAGE_IMPORT = "import org.drools.benchmarks.oopath.model.*;";
+
+    @Param({"10"})
+    protected int numberOfParentFacts;
+
+    protected List<Man> parentFacts;
+    protected List<Child> factsToBeModified;
+    protected List<String> globalList;
+
+    public abstract void setupKieBase();
+
+    @Setup(Level.Iteration)
+    @Override
+    public void setup() throws ProviderException {
+        createKieSession();
+        globalList = new ArrayList<>();
+        kieSession.setGlobal("list", globalList);
+        prepareFacts();
+    }
+
+    private void prepareFacts() {
+        parentFacts = generateModel(numberOfParentFacts);
+        factsToBeModified = getChildToBeModified(parentFacts);
+    }
+
+    private static List<Man> generateModel(final int nr) {
+        final List<Man> model = new ArrayList<>();
+        for (int i = 0; i < nr; i++) {
+            final Man man = new Man("m" + i, 40);
+            model.add(man);
+            final Woman woman = new Woman("w" + i, 35);
+            man.setWife(woman);
+            woman.setHusband(man.getName());
+
+            final Child childA = new Child("cA" + i, 12);
+            woman.addChild(childA);
+            childA.setMother(woman.getName());
+            final Child childB = new Child("cB" + i, 10);
+            woman.addChild(childB);
+            childB.setMother(woman.getName());
+
+            final Toy toyA = new Toy("tA" + i);
+            toyA.setOwner(childA.getName());
+            childA.addToy(toyA);
+            final Toy toyB = new Toy("tB" + i);
+            toyB.setOwner(childA.getName());
+            childA.addToy(toyB);
+            final Toy toyC = new Toy("tC" + i);
+            toyC.setOwner(childB.getName());
+            childB.addToy(toyC);
+        }
+        return model;
+    }
+
+    private static List<Child> getChildToBeModified(final List<Man> model) {
+        final List<Child> toBeModified = new ArrayList<>();
+        for (final Man man : model) {
+            for (final Child child : man.getWife().getChildren()) {
+                if (child.getAge() == 10) {
+                    toBeModified.add(child);
+                }
+            }
+        }
+        return toBeModified;
+    }
+
+    protected static List<InternalFactHandle> insertModel(final KieSession ksession, final List<Man> model) {
+        final List<InternalFactHandle> fhs = new ArrayList<>();
+        for (final Man man : model) {
+            fhs.add((InternalFactHandle)ksession.insert(man));
+        }
+        return fhs;
+    }
+
+    protected static List<InternalFactHandle> insertFullModel(final KieSession ksession, final List<Man> model) {
+        final List<InternalFactHandle> toBeModified = new ArrayList<>();
+        for (final Man man : model) {
+            ksession.insert(man);
+            ksession.insert(man.getWife());
+            for (final Child child : man.getWife().getChildren()) {
+                final InternalFactHandle fh = (InternalFactHandle)ksession.insert(child);
+                if (child.getAge() == 10) {
+                    toBeModified.add(fh);
+                }
+                for (final Toy toy : child.getToys()) {
+                    ksession.insert(toy);
+                }
+            }
+        }
+        return toBeModified;
+    }
+}
---
+++ drools-benchmarks/src/main/java/org/drools/benchmarks/oopath/OOPathComparisonFromVariationUpdateBenchmark.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.benchmarks.oopath;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.drools.core.common.InternalFactHandle;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.Warmup;
+
+public class OOPathComparisonFromVariationUpdateBenchmark extends AbstractOOPathComparisonBenchmark {
+
+    @Setup
+    @Override
+    public void setupKieBase() {
+        final String drl = MODEL_PACKAGE_IMPORT + "\n" +
+                "global java.util.List list\n" +
+                "\n" +
+                "rule R when\n" +
+                "    $man: Man( $wife: wife )\n" +
+                "    $child: Child( age > 10 ) from $wife.children\n" +
+                "    $toy: Toy() from $child.toys\n" +
+                "then\n" +
+                "    list.add( $toy.getName() );\n" +
+                "end\n";
+        createKieBaseFromDrl(drl);
+    }
+
+    private List<InternalFactHandle> factHandles;
+
+    @Setup(Level.Iteration)
+    public void insertFacts() {
+        factHandles = insertModel(kieSession, parentFacts);
+        kieSession.fireAllRules();
+        globalList.clear();
+    }
+
+    @Benchmark
+    public int testUpdate() {
+        factsToBeModified.forEach(child -> child.setAge(11));
+        factHandles.forEach(factHandle -> kieSession.update(factHandle, factHandle.getObject()));
+        return kieSession.fireAllRules();
+    }
+}
