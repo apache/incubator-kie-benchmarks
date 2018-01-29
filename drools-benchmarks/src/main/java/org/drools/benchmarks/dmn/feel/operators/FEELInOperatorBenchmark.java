8c716ef1e440fc86b3c877d3c8b0f91d674e9ba2
tzimanyi <tibor.zimanyi@gmail.com>
2018-01-29 12:54:46 +0100

    Add first DMN FEEL evaluation benchmarks

---
+++ drools-benchmarks/src/main/java/org/drools/benchmarks/dmn/feel/operators/FEELInOperatorBenchmark.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.benchmarks.dmn.feel.operators;
+
+import org.drools.benchmarks.dmn.feel.AbstractFEELBenchmark;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+
+public class FEELInOperatorBenchmark extends AbstractFEELBenchmark {
+
+    @Param({"10 in ( 3, 10, 20 )",
+            "\"foo\" in ( 10, false, \"foo\" )",
+            "10 in ( > 50, < 5 )",
+            "10 in (5..10]"})
+    private String expression;
+
+    @Benchmark
+    public Object evaluateExpressionBenchmark() {
+        return evaluateFEELExpression(expression);
+    }
+}
---
+++ drools-benchmarks/src/main/java/org/drools/benchmarks/dmn/feel/operators/FEELNotOperatorBenchmark.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.benchmarks.dmn.feel.operators;
+
+import org.drools.benchmarks.dmn.feel.AbstractFEELBenchmark;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+
+public class FEELNotOperatorBenchmark extends AbstractFEELBenchmark {
+
+    @Param({"not( true )",
+            "not( 10 = 3 )",
+            "not( \"foo\" )"})
+    private String expression;
+
+    @Benchmark
+    public Object evaluateExpressionBenchmark() {
+        return evaluateFEELExpression(expression);
+    }
+}
