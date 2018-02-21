81c24eed9196746e4c0ce14d11b79adbc54afe9f
tzimanyi <tibor.zimanyi@gmail.com>
2018-02-21 14:51:04 +0100

    Align BuildtimeUtil with latest Drools API changes

--- drools-benchmarks/src/main/java/org/drools/benchmarks/common/util/BuildtimeUtil.java
+++ drools-benchmarks/src/main/java/org/drools/benchmarks/common/util/BuildtimeUtil.java
@@ -25,7 +25,7 @@ import org.drools.compiler.kie.builder.impl.InternalKieModule;
 import org.drools.compiler.kie.builder.impl.KieBuilderImpl;
 import org.drools.compiler.kie.builder.impl.ZipKieModule;
 import org.drools.modelcompiler.CanonicalKieModule;
-import org.drools.modelcompiler.CanonicalModelProject;
+import org.drools.modelcompiler.CanonicalModelPatternProject;
 import org.kie.api.KieBase;
 import org.kie.api.KieBaseConfiguration;
 import org.kie.api.KieServices;
@@ -84,7 +84,7 @@ public final class BuildtimeUtil {
 
         final KieBuilderImpl kbuilder = (KieBuilderImpl) KieServices.Factory.get().newKieBuilder(kfs);
         if (useCanonicalModel) {
-            kbuilder.buildAll(CanonicalModelProject.class);
+            kbuilder.buildAll(CanonicalModelPatternProject.class);
         } else {
             kbuilder.buildAll();
         }
