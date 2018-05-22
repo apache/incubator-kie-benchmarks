3ce7417011a710d1297bb508ecea669e1bbc97ae
Marián Macik <mmacik@redhat.com>
2018-05-22 13:04:15 +0200

    Bump version for kieserver-assets project

--- jbpm-benchmarks/kieserver-performance-tests/src/main/java/org/jbpm/test/performance/kieserver/KieServerClient.java
+++ jbpm-benchmarks/kieserver-performance-tests/src/main/java/org/jbpm/test/performance/kieserver/KieServerClient.java
@@ -25,7 +25,7 @@ public class KieServerClient {
 	public static String containerId = "kieserver-assets";
 	
     private static ReleaseId releaseId = new ReleaseId("org.jbpm.test.performance", "kieserver-assets",
-            "7.3.0-SNAPSHOT");
+            "7.7.0-SNAPSHOT");
 
     private ProcessServicesClient processClient;
     private UserTaskServicesClient taskClient;
