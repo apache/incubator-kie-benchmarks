47602622a153d9ba3b93468759c0f120d49a37c3
tzimanyi <tibor.zimanyi@gmail.com>
2018-08-23 16:16:17 +0200

    Update to 7.11.0-SNAPSHOT

--- jbpm-benchmarks/kieserver-performance-tests/src/main/java/org/jbpm/test/performance/kieserver/KieServerClient.java
+++ jbpm-benchmarks/kieserver-performance-tests/src/main/java/org/jbpm/test/performance/kieserver/KieServerClient.java
@@ -27,7 +27,7 @@ public class KieServerClient {
 	public static String containerId = "kieserver-assets";
 	
     private static ReleaseId releaseId = new ReleaseId("org.jbpm.test.performance", "kieserver-assets",
-            "7.10.0-SNAPSHOT");
+            "7.11.0-SNAPSHOT");
 
     private ProcessServicesClient processClient;
     private UserTaskServicesClient taskClient;
