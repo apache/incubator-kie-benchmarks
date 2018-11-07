1280c15be7e81f558ea8b06f12a30077eb412659
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2018-11-07 05:46:35 +0100

    upgraded to 7.15.0-SNAPSHOT

--- jbpm-benchmarks/kieserver-performance-tests/src/main/java/org/jbpm/test/performance/kieserver/KieServerClient.java
+++ jbpm-benchmarks/kieserver-performance-tests/src/main/java/org/jbpm/test/performance/kieserver/KieServerClient.java
@@ -27,7 +27,7 @@ public class KieServerClient {
 	public static String containerId = "kieserver-assets";
 	
     private static ReleaseId releaseId = new ReleaseId("org.jbpm.test.performance", "kieserver-assets",
-            "7.14.0-SNAPSHOT");
+            "7.15.0-SNAPSHOT");
 
     private ProcessServicesClient processClient;
     private UserTaskServicesClient taskClient;
