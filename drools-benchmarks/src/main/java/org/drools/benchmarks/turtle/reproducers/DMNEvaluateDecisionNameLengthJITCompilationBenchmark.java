1b7eaaaf2f3b6d4bfa64acdf192c6158358166f9
tarilabs <matteo.mortari@gmail.com>
2018-12-10 13:47:44 +0100

    .

--- drools-benchmarks/src/main/java/org/drools/benchmarks/turtle/reproducers/DMNEvaluateDecisionNameLengthJITCompilationBenchmark.java
+++ drools-benchmarks/src/main/java/org/drools/benchmarks/turtle/reproducers/DMNEvaluateDecisionNameLengthJITCompilationBenchmark.java
@@ -37,6 +37,7 @@ import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Warmup;
 
 @Warmup(iterations = 30)
@@ -62,6 +63,11 @@ public class DMNEvaluateDecisionNameLengthJITCompilationBenchmark extends Abstra
         System.setProperty("org.kie.dmn.compiler.execmodel", execModelProperty);
         dmnResource = KieServices.get().getResources().newClassPathResource("dmn/" + dmnFileName).setResourceType(ResourceType.DMN);
         dmnRuntime = DMNUtil.getDMNRuntimeWithResources(false, dmnResource);
+    }
+
+    @TearDown
+    public void tearDown() {
+        System.clearProperty("org.kie.dmn.compiler.execmodel");
     }
 
     @Setup(Level.Iteration)
