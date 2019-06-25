32f6419af84960461ef29cabcac7597d5f61c844
tarilabs <matteo.mortari@gmail.com>
2019-06-25 14:14:26 +0200

    cleanup

--- drools-benchmarks/src/main/java/org/drools/benchmarks/dmn/runtime/DMNEvaluateDecisionTablesSparseBenchmark.java
+++ drools-benchmarks/src/main/java/org/drools/benchmarks/dmn/runtime/DMNEvaluateDecisionTablesSparseBenchmark.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -110,91 +110,3 @@ public class DMNEvaluateDecisionTablesSparseBenchmark extends AbstractBenchmark 
     }
 }
 
-/*
-BASELINE
-Result "org.drools.benchmarks.dmn.runtime.DMNEvaluateDecisionTablesSparseBenchmark.evaluateDecision":
-  N = 250
-  mean =     18.447 ±(99.9%) 0.633 ms/op
-
-  Histogram, ms/op:
-    [10.000, 12.500) = 0 
-    [12.500, 15.000) = 13 
-    [15.000, 17.500) = 105 
-    [17.500, 20.000) = 78 
-    [20.000, 22.500) = 35 
-    [22.500, 25.000) = 12 
-    [25.000, 27.500) = 3 
-    [27.500, 30.000) = 2 
-    [30.000, 32.500) = 0 
-    [32.500, 35.000) = 1 
-    [35.000, 37.500) = 1 
-
-  Percentiles, ms/op:
-      p(0.0000) =     14.240 ms/op
-     p(50.0000) =     17.667 ms/op
-     p(90.0000) =     21.888 ms/op
-     p(95.0000) =     23.687 ms/op
-     p(99.0000) =     31.485 ms/op
-     p(99.9000) =     35.549 ms/op
-     p(99.9900) =     35.549 ms/op
-     p(99.9990) =     35.549 ms/op
-     p(99.9999) =     35.549 ms/op
-    p(100.0000) =     35.549 ms/op
-
-
-# Run complete. Total time: 00:03:30
-
-REMEMBER: The numbers below are just data. To gain reusable insights, you need to follow up on
-why the numbers are the way they are. Use profilers (see -prof, -lprof), design factorial
-experiments, perform baseline and negative tests that provide experimental control, make sure
-the benchmarking environment is safe on JVM/OS/HW level, ask for reviews from the domain experts.
-Do not assume the numbers tell you what you want them to tell.
-
-Benchmark                                                  (numberOfElements)  (sparseness)  Mode  Cnt   Score   Error  Units
-DMNEvaluateDecisionTablesSparseBenchmark.evaluateDecision                 100            10    ss  250  18.447 ± 0.633  ms/op
-
-AFTER
-Result "org.drools.benchmarks.dmn.runtime.DMNEvaluateDecisionTablesSparseBenchmark.evaluateDecision":
-  N = 250
-  mean =      2.744 ±(99.9%) 0.180 ms/op
-
-  Histogram, ms/op:
-    [1.000, 1.500) = 0 
-    [1.500, 2.000) = 4 
-    [2.000, 2.500) = 136 
-    [2.500, 3.000) = 50 
-    [3.000, 3.500) = 22 
-    [3.500, 4.000) = 17 
-    [4.000, 4.500) = 10 
-    [4.500, 5.000) = 4 
-    [5.000, 5.500) = 3 
-    [5.500, 6.000) = 1 
-    [6.000, 6.500) = 1 
-    [6.500, 7.000) = 1 
-    [7.000, 7.500) = 0 
-
-  Percentiles, ms/op:
-      p(0.0000) =      1.965 ms/op
-     p(50.0000) =      2.438 ms/op
-     p(90.0000) =      3.921 ms/op
-     p(95.0000) =      4.450 ms/op
-     p(99.0000) =      6.486 ms/op
-     p(99.9000) =      7.875 ms/op
-     p(99.9900) =      7.875 ms/op
-     p(99.9990) =      7.875 ms/op
-     p(99.9999) =      7.875 ms/op
-    p(100.0000) =      7.875 ms/op
-
-
-# Run complete. Total time: 00:03:12
-
-REMEMBER: The numbers below are just data. To gain reusable insights, you need to follow up on
-why the numbers are the way they are. Use profilers (see -prof, -lprof), design factorial
-experiments, perform baseline and negative tests that provide experimental control, make sure
-the benchmarking environment is safe on JVM/OS/HW level, ask for reviews from the domain experts.
-Do not assume the numbers tell you what you want them to tell.
-
-Benchmark                                                  (numberOfElements)  (sparseness)  Mode  Cnt  Score   Error  Units
-DMNEvaluateDecisionTablesSparseBenchmark.evaluateDecision                 100            10    ss  250  2.744 ± 0.180  ms/op
-
- */
