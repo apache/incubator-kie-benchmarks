34b234d75b04907ef5564afd1aa5a518419b6ca4
Daniel Rosa <hellowdan@gmail.com>
2019-10-09 15:01:57 +0200

    BAQE-1112 - Create new runtime benchmark using decision tables (#59)

---
+++ drools-benchmarks/src/main/java/org/drools/benchmarks/turtle/runtime/DTable1Benchmark.java
@@ -0,0 +1,41 @@
+package org.drools.benchmarks.turtle.runtime;
+
+import org.drools.benchmarks.model.Account;
+import org.kie.api.runtime.KieSession;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.Benchmark;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Warmup(iterations = 100000)
+@Measurement(iterations = 10000)
+public class DTable1Benchmark extends AbstractSimpleRuntimeBenchmark {
+
+    private static int from = 999;
+    private static int to = 3102;
+
+    @Setup
+    public void addResources() {
+        addClassPathResource("kbase-creation/dtable1-kbase-creation.xls");
+    }
+
+    @Benchmark
+    public KieSession timeFactsInsertionAndRulesFiring() {
+        return insertFactsAndFireAllRules();
+    }
+
+    @Override
+    protected List<Object> generateFacts() {
+        List<Object> facts = new ArrayList<Object>();
+        for (int i = from; i < to; i++) {
+            Account account = new Account();
+            account.setNumber(i);
+            facts.add(account);
+        }
+        return facts;
+    }
+
+}
---
+++ drools-benchmarks/src/main/java/org/drools/benchmarks/turtle/runtime/DTable2Benchmark.java
@@ -0,0 +1,35 @@
+package org.drools.benchmarks.turtle.runtime;
+
+import org.drools.benchmarks.turtle.runtime.generator.GeneratorConfiguration;
+import org.drools.benchmarks.turtle.runtime.generator.KBaseCreationFromDTable2Generator;
+import org.kie.api.runtime.KieSession;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.Benchmark;
+
+@Warmup(iterations = 2000)
+@Measurement(iterations = 200)
+public class DTable2Benchmark extends AbstractSimpleRuntimeBenchmark {
+
+    @Setup
+    public void addResources() {
+        addClassPathResource("kbase-creation/dtable2-kbase-creation.xls");
+    }
+
+    @Override
+    protected void addFactsGenerators() {
+        addFactsGenerator(new KBaseCreationFromDTable2Generator(getGeneratorConfiguration()),20000);
+    }
+
+    @Benchmark
+    public KieSession timeFactsInsertionAndRulesFiring() {
+        return insertFactsAndFireAllRules();
+    }
+
+    private GeneratorConfiguration getGeneratorConfiguration() {
+        final GeneratorConfiguration generatorConfiguration = new GeneratorConfiguration(
+                1490, 5, 0.5);
+        return generatorConfiguration;
+    }
+}
