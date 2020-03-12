27183157285276772f4e2da4efb7224e976da4b6
Michal Cervenansky <mcervena@redhat.com>
2020-03-12 09:10:38 +0100

    JMH Jbpm performance tests completed and should be ready to try on Jenkins.

---
+++ jbpm-benchmarks/jbpm-performance-tests-jmh/src/main/java/org/jbpm/test/performance/scenario/load/L1000HumanTasksComplete.java
@@ -0,0 +1,66 @@
+package org.jbpm.test.performance.scenario.load;
+
+import org.jbpm.test.performance.jbpm.JBPMController;
+import org.jbpm.test.performance.jbpm.constant.UserStorage;
+import org.jbpm.test.performance.scenario.PrepareEngine;
+import org.kie.api.task.TaskService;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+@Fork(value = 1, jvmArgs = {"-Xms2G", "-Xmx2G"})
+@Warmup(iterations = 1, time = 1)
+@Measurement(iterations = 1, time = 5)
+@Threads(1)
+
+public class L1000HumanTasksComplete {
+
+    private JBPMController jc;
+
+    private TaskService taskService;
+
+    private List<Long> taskIds;
+
+    @Setup
+    public void init() {
+
+        jc = JBPMController.getInstance();
+        jc.createRuntimeManager();
+
+        taskService = jc.getRuntimeEngine().getTaskService();
+
+        taskId = 0;
+        taskIds = PrepareEngine.createNewTasks(true, 5000, taskService, jc.getRuntimeManagerIdentifier());
+    }
+
+    static int taskId = 0;
+
+
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Benchmark
+    public void Throughput() {
+        execute();
+    }
+
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @Benchmark
+    public void averageTime() {
+        execute();
+    }
+
+    private void execute() {
+        Long tid = taskIds.get(taskId);
+        taskService.complete(tid, UserStorage.PerfUser.getUserId(), null);
+        taskId++;
+    }
+
+    @TearDown
+    public void close() {
+        jc.tearDown();
+    }
+
+}
