a8353542a845516fb241afe4e2259f67e83f91ec
Anna Dupliak <dupliaka@gmail.com>
2021-08-11 10:36:49 +0200

    Change usage instruction

--- update-version.sh
+++ update-version.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-if [[ $# != 1 || $1 = -h || $1 = --help ]]
+if [[ $# != 2 || $1 = -h || $1 = --help ]]
 then
-  echo "Usage: $0 [OPTIONS] <new_version>"
+  echo "Usage: $0 [OPTIONS] <new_kie_version> <new_optaplanner_version>"
   echo ""
   echo "Updates version of this project, including parents of individual modules."
   echo ""
   echo "Example:"
   echo ""
-  echo "$0 7.26.0.redhat-000004"
+  echo "$0 7.26.0.redhat-000004 8.5.0.Final-redhat-00004"
   echo ""
   echo "OPTIONS"
   echo " -h, --help      display this help"
