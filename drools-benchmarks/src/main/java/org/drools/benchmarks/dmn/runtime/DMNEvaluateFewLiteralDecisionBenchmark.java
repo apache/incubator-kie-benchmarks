c8ef78e0b3f8b40b43ea7f20c1b1fae8021dcd52
Matteo Mortari <matteo.mortari@gmail.com>
2021-12-06 15:27:54 +0100

    DROOLS-6725 DMN DMNLiteralExpressionEvaluator to internalize FEEL (#151)
    * DMNLiteralExpr to internalize FEEL
    
    * created DMNEvaluateFewLiteralDecisionBenchmark
    
    as DMNEvaluateDecisionBenchmark does not show
    meaningful numbers for a realistic scenario
    
    * .

---
+++ drools-benchmarks/src/main/java/org/drools/benchmarks/dmn/runtime/DMNEvaluateFewLiteralDecisionBenchmark.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.benchmarks.dmn.runtime;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.drools.benchmarks.common.AbstractBenchmark;
+import org.drools.benchmarks.common.DMNProvider;
+import org.drools.benchmarks.common.ProviderException;
+import org.drools.benchmarks.common.providers.dmn.DecisionDMNProvider;
+import org.drools.benchmarks.dmn.util.DMNUtil;
+import org.kie.api.KieServices;
+import org.kie.api.io.Resource;
+import org.kie.api.io.ResourceType;
+import org.kie.dmn.api.core.DMNContext;
+import org.kie.dmn.api.core.DMNModel;
+import org.kie.dmn.api.core.DMNResult;
+import org.kie.dmn.api.core.DMNRuntime;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.Warmup;
+
+@Warmup(iterations = 5000)
+@Measurement(iterations = 2000)
+public class DMNEvaluateFewLiteralDecisionBenchmark extends AbstractBenchmark {
+
+    @Param({"20", "50", "100"})
+    private int numberOfDecisions;
+
+    private Resource dmnResource;
+    private DMNRuntime dmnRuntime;
+    private DMNModel dmnModel;
+    private DMNContext dmnContext;
+
+    @Setup
+    public void setupResource() throws IOException {
+        final DMNProvider dmnProvider = new DecisionDMNProvider();
+        dmnResource = KieServices.get().getResources()
+                .newReaderResource(new StringReader(dmnProvider.getDMN(numberOfDecisions)))
+                .setResourceType(ResourceType.DMN)
+                .setSourcePath("dmnFile.dmn");
+        dmnRuntime = DMNUtil.getDMNRuntimeWithResources(false, dmnResource);
+        dmnModel = dmnRuntime.getModel("https://github.com/kiegroup/drools/kie-dmn", "decision");
+    }
+
+    @Setup(Level.Iteration)
+    @Override
+    public void setup() throws ProviderException {
+        dmnContext = dmnRuntime.newContext();
+        dmnContext.set("Full Name", "John Doe");
+    }
+
+    @Benchmark
+    public DMNResult evaluateDecision() {
+        return dmnRuntime.evaluateAll(dmnModel, dmnContext);
+    }
+}
