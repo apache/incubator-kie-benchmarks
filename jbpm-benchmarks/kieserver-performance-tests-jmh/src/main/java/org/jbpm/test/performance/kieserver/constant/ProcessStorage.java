4b9b76298041b60cb648b7e412b38bd471e93f93
Martin Weiler <mweiler@redhat.com>
2023-05-11 15:14:45 -0600

    [RHPAM-4698] Benchmark test for task notifications

--- jbpm-benchmarks/kieserver-performance-tests-jmh/src/main/java/org/jbpm/test/performance/kieserver/constant/ProcessStorage.java
+++ jbpm-benchmarks/kieserver-performance-tests-jmh/src/main/java/org/jbpm/test/performance/kieserver/constant/ProcessStorage.java
@@ -4,7 +4,7 @@ public enum ProcessStorage {
 
     HumanTask("org.kie.perf.HumanTask"), HumanTaskTimer("org.kie.perf.HumanTaskTimer"), GroupHumanTask("org.kie.perf.GroupHumanTask"), ParallelGatewayTwoTimes(
             "org.kie.perf.ParallelGatewayTwoTimes"), RuleTask("org.kie.perf.RuleTask"), StartEnd("org.kie.perf.StartEnd"), IntermediateTimer(
-            "org.kie.perf.IntermediateTimer");
+            "org.kie.perf.IntermediateTimer"), HumanTaskNotification("org.kie.perf.HumanTaskNotification");
 
     private String processDefinitionId;
 
