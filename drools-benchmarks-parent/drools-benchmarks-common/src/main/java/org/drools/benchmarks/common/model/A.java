25381b903a4f81799a50f664288bfafeadeb928d
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2023-10-24 22:58:06 +0900

    [DROOLS-7576] Add benchmarks for secondary super cache issue (#270)
    * [DROOLS-7576] Add benchmarks for secondary super cache issue
    - ConcurrentUpdateRightTupleBenchmark calls RightTupleImpl.getTupleSink() a lot
    - ConcurrentUpdateLeftTupleBenchmark calls LeftTuple.getTupleSink() a lot
    
    * - add comment

--- drools-benchmarks-parent/drools-benchmarks-common/src/main/java/org/drools/benchmarks/common/model/A.java
+++ drools-benchmarks-parent/drools-benchmarks-common/src/main/java/org/drools/benchmarks/common/model/A.java
@@ -29,4 +29,14 @@ public class A extends AbstractBean {
     public A(final long id, final int value) {
         super(id, value);
     }
+
+    private int value2;
+
+    public int getValue2() {
+        return value2;
+    }
+
+    public void setValue2(int value2) {
+        this.value2 = value2;
+    }
 }
