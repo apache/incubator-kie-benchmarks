acaae15f37629aed27d92a9ac7eea7340b3a8331
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2023-12-20 11:08:24 +0100

    [incubator-kie-issues#780] DMN - Benchmarking InfixOpNode and InfixOperators (#281)
    * [dmn-infixopnode-benchmark] Benchmarking infixopnode
    
    * [incubator-kie-issues#781] Benchmarking infixopnode
    
    * [incubator-kie-issues#781] Benchmarking infixexecutors
    
    * [incubator-kie-issues#781] Fix as per PR suggestion
    
    * [incubator-kie-issues#780] Benchmarking infixopnode
    
    * [incubator-kie-issues#780] Benchmarking infixexecutors
    
    * [incubator-kie-issues#780] Fix as per PR suggestion
    
    ---------
    
    Co-authored-by: BAMOE CI <bamoe.ci@ibm.com>

---
+++ drools-benchmarks-parent/drools-benchmarks/src/main/java/org/drools/benchmarks/dmn/feel/infixexecutors/FEELGtExecutorBenchmark.java
@@ -0,0 +1,58 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.drools.benchmarks.dmn.feel.infixexecutors;
+
+import org.kie.dmn.feel.lang.EvaluationContext;
+import org.kie.dmn.feel.lang.ast.infixexecutors.GtExecutor;
+import org.kie.dmn.feel.lang.impl.EvaluationContextImpl;
+import org.kie.dmn.feel.util.ClassLoaderUtil;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.drools.benchmarks.dmn.feel.infixexecutors.FEELInfixExecutorBenchmarkUtils.getBooleanArray;
+
+@Warmup(iterations = 10)
+@Measurement(iterations = 50)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(1)
+@State(Scope.Thread)
+public class FEELGtExecutorBenchmark {
+
+    private GtExecutor executor;
+    private EvaluationContext ctx;
+
+    private Object[] values;
+
+    @Param({"String String", "Duration Duration", "int int", "ChronoPeriod ChronoPeriod"})
+    private String args;
+
+
+    @Setup
+    public void setup() {
+        executor = GtExecutor.instance();
+        ctx = new EvaluationContextImpl(ClassLoaderUtil.findDefaultClassLoader(), null);
+        values = getBooleanArray(args);
+    }
+
+    @Benchmark
+    public Object evaluate() {
+        return executor.evaluate(values[0], values[1], ctx);
+    }
+}
---
+++ drools-benchmarks-parent/drools-benchmarks/src/main/java/org/drools/benchmarks/dmn/feel/infixexecutors/FEELPowExecutorBenchmark.java
@@ -0,0 +1,58 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.drools.benchmarks.dmn.feel.infixexecutors;
+
+import org.kie.dmn.feel.lang.EvaluationContext;
+import org.kie.dmn.feel.lang.ast.infixexecutors.PowExecutor;
+import org.kie.dmn.feel.lang.impl.EvaluationContextImpl;
+import org.kie.dmn.feel.util.ClassLoaderUtil;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.drools.benchmarks.dmn.feel.infixexecutors.FEELInfixExecutorBenchmarkUtils.getObjectArray;
+
+@Warmup(iterations = 10)
+@Measurement(iterations = 50)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(1)
+@State(Scope.Thread)
+public class FEELPowExecutorBenchmark {
+
+    private PowExecutor executor;
+    private EvaluationContext ctx;
+
+    private Object[] values;
+
+    @Param({"int int"})
+    private String argsType;
+
+
+    @Setup
+    public void setup() {
+        executor = PowExecutor.instance();
+        ctx = new EvaluationContextImpl(ClassLoaderUtil.findDefaultClassLoader(), null);
+        values = getObjectArray(argsType);
+    }
+
+    @Benchmark
+    public Object evaluate() {
+        return executor.evaluate(values[0], values[1], ctx);
+    }
+}
